22f1edfecc91396e10eb50b21ed4fdca05a76b45
Elias Platek <eplatek@lamobilery.fr>
2020-04-18 15:10:15 +0200

    Updated dependencies

--- src/context.ts
+++ src/context.ts
@@ -4,29 +4,12 @@ interface ContextState {
   currentValue: number
 }
 
-const { createAction, createActions, useContext, Provider } = buildContext<
-  ContextState
->()
+const { createActions, useContext, Provider } = buildContext<ContextState>({
+  useDebug: true,
+})
 
 export const useAppContext = useContext
 export const AppContextProvider = Provider
-
-export const setValueAction = createAction(
-  (value: number) => async ({ produceState, getState }) => {
-    const state = getState()
-    await produceState((draft) => {
-      draft.currentValue = state.currentValue + value
-    })
-  }
-)
-
-export const incrementAction = createAction(() => async ({ getState }) => {
-  await getState().dispatch(setValueAction(1))
-})
-
-export const decrementAction = createAction(() => async ({ getState }) => {
-  await getState().dispatch(setValueAction(-1))
-})
 
 export const actions = createActions({
   setValue: (value: number) => async ({ produceState, getState }) => {
